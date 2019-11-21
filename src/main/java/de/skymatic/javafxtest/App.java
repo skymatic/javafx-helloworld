0aa219400c35fc85f96f8d8aa027814eecdd279e
Armin Schrenk <armin.schrenk@zoho.eu>
2019-11-21 13:21:48 +0100

    adding test button to simple fx app

--- src/main/java/de/skymatic/javafxtest/App.java
+++ src/main/java/de/skymatic/javafxtest/App.java
@@ -1,9 +1,12 @@
 package de.skymatic.javafxtest;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -17,7 +20,11 @@ public class App extends Application {
         var javafxVersion = SystemInfo.javafxVersion();
 
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        var button = new Button("Test Button");
+        var vbox = new VBox(label, button);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(10);
+        var scene = new Scene(vbox, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
