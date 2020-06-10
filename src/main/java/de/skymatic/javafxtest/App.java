251012807491c95d203b9d2e292a3f8be4e307da
Armin Schrenk <armin.schrenk@zoho.eu>
2020-06-10 16:28:52 +0200

    Add text input field to window

--- src/main/java/de/skymatic/javafxtest/App.java
+++ src/main/java/de/skymatic/javafxtest/App.java
@@ -1,11 +1,12 @@
 package de.skymatic.javafxtest;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -20,8 +21,11 @@ public class App extends Application {
         var javafxVersion = SystemInfo.javafxVersion();
 
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        var textInputFiled = new TextField();
+        textInputFiled.setPromptText("Test text field: Enter anything you want.");
         var button = new Button("Test Button");
-        var vbox = new VBox(label, button);
+        var vbox = new VBox(label, textInputFiled, button);
+        vbox.setPadding(new Insets(20));
         vbox.setAlignment(Pos.CENTER);
         vbox.setSpacing(10);
         var scene = new Scene(vbox, 640, 480);
